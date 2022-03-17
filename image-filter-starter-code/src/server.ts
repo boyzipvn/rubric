527517ad2ca478279c05a3b2a19b1db7ac4f7606
boyzipvn <manhvv1993@gmail.com>
2022-03-17 16:45:16 +0900

    coding todo

--- image-filter-starter-code/src/server.ts
+++ image-filter-starter-code/src/server.ts
@@ -9,7 +9,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -30,13 +30,26 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  app.get( "/filteredimage", async ( req, res) => {
+
+    let { image_url } = req.query;
+
+    if ( !image_url ) {
+      return res.status(400).send('image_url is required');
+    }
+
+    const filteredpath = await filterImageFromURL(image_url);
+    let list_files = [filteredpath];
+    res.sendFile(filteredpath, function(){
+      deleteLocalFiles(list_files);
+    });
+
+    //res.send("try GET /filteredimage?image_url={{}}")
+  });
+
 
   // Start the Server
   app.listen( port, () => {
